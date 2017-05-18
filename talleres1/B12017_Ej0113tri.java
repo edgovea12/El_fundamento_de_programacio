a69f1fca1302cc29c712c09cea35e8e05eea6a28
edgovea12 <kenichigovea_94@hotmail.com>
2017-05-18 17:00:48 -0500

    Add files via upload

---
+++ talleres1/B12017_Ej0113tri.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tes.edu.ec;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ *
+ * @author alum_tes_a
+ */
+public class B12017_Ej0113tri 
+{
+    public static void main(String args [])
+    {
+        // Cuadrado
+        int n = 5;
+
+		for(int i=0;i<n;i++)
+		{
+			for(int j=0;j<n;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}
+    }
+}
---
+++ talleres1/B12017_Ej02_If.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tes.edu.ec;
+
+/**
+ *
+ * @author alum_tes_a
+ */
+public class B12017_Ej02_If 
+{
+    public static void main(String args [])
+    {
+        int edad = 21;
+        if (edad >= 18)
+        {
+            System.out.println("Eres mayor de edad.");
+        }
+        else
+        {
+            System.out.println("eres menor de edad");
+        }          
+    }
+    
+}
